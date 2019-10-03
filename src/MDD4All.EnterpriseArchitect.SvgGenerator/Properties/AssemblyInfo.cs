73b9b2e82bc43174028d59013c89d8d72235a9c8
oalt <oa@lacheweg21.de>
2019-10-03 16:00:15 +0200

    Adding files necessary for CI pipelines

--- src/MDD4All.EnterpriseArchitect.SvgGenerator/Properties/AssemblyInfo.cs
+++ src/MDD4All.EnterpriseArchitect.SvgGenerator/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
 [assembly: AssemblyTitle("MDD4All.EnterpriseArchitect.SvgGenerator")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("FMC4SE SVG Generator")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("MDD4All.de")]
 [assembly: AssemblyProduct("MDD4All.EnterpriseArchitect.SvgGenerator")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("© MDD4All.de")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
