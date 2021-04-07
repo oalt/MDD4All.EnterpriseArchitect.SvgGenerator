4d91113aec481a710f075416c330232c24c8b741
oalt <oa@lacheweg21.de>
2021-04-07 13:57:20 +0200

    Remove reference to SpecIF diagram interchange

--- src/MDD4All.SVG.DataModels/Metadata.cs
+++ src/MDD4All.SVG.DataModels/Metadata.cs
@@ -1,19 +1,12 @@
 ﻿/*
  * Copyright (c) MDD4All.de, Dr. Oliver Alt
  */
-using MDD4All.SpecIF.DataModels.DiagramInterchange;
 using System.Collections.Generic;
-using System.Xml.Serialization;
 
 namespace MDD4All.SVG.DataModels
 {
     public class Metadata
     {
 
-        [XmlElement("shape", Namespace = "https://specif.de/schema/v1.0/DI")]
-        public Shape Shape { get; set; } = null;
-
-        [XmlElement("edge", Namespace = "https://specif.de/schema/v1.0/DI")]
-        public Edge Edge { get; set; } = null;
     }
 }
