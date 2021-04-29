34f427fcb8e32fa23a4e37f41885a50437b54a86
oalt <oa@lacheweg21.de>
2021-04-29 09:39:32 +0200

    Fixing build problem

--- src/MDD4All.EnterpriseArchitect.SvgGenerator/DataModels/LinkGeometry.cs
+++ src/MDD4All.EnterpriseArchitect.SvgGenerator/DataModels/LinkGeometry.cs
@@ -1,11 +1,11 @@
 ﻿/*
  * Copyright (c) MDD4All.de, Dr. Oliver Alt
  */
-using System;
-using System.Collections.Generic;
-using System.Text;
-
+#if EA_FACADE
+namespace MDD4All.EAFacade.SvgGenerator.DataModels
+#else
 namespace MDD4All.EnterpriseArchitect.SvgGenerator.DataModels
+#endif
 {
     public class LinkGeometry
     {
